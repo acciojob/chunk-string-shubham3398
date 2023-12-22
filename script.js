45ba4d6e8a9cd31bd7120ea6d4fc46e7239c02ca
shubham3398 <117896201+shubham3398@users.noreply.github.com>
2023-12-22 17:24:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 function stringChop(str, size) {
   // your code here
+  if (size >= str.length) return [str];
+
+  let i = 0;
+  let ans = [];
+  while (i < str.length) {
+    let lastIndex = Math.min(i + size, str.length); // Use Math.min to avoid going beyond the string length
+    ans.push(str.substring(i, lastIndex));
+    i += size;
+  }
+  return ans;
 }
 
 // Do not change the code below
 const str = prompt("Enter String.");
 const size = prompt("Enter Chunk Size.");
-alert(stringChop(str, size));
+alert(stringChop(str, size)); 
